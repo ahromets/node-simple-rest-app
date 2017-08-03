0c677f4131fe194bf26adcbdae97a2d47789cf9b
Alexey Khromets <alexchromets@gmail.com>
2017-08-03 14:48:03 +0300

    add gulp server task

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # node-simple-rest-app
 1. Run server: `node server/server.js`.
 2. Go to `localhost:7000/api/items/` to get the items
-3. To run test Gulp tasks:
-    1. In CLI type `gulp` to run the default task
-    2. In CLI type `gulp server` to run the server task
+3. To run server from Gulp in CLI type `gulp server` to run the server task
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 var gulp = require('gulp');
+var spawn = require('child_process').spawn;
+var node;
 
 gulp.task('default', function () {
     console.log('This is a default task');
 });
 
 gulp.task('server', function () {
-    console.log('This is a server task');
+    if (node) node.kill();
+    node = spawn('node', ['./server/server.js'], {stdio: 'inherit'});
 });
