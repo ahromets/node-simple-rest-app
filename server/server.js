1b31fd584b12ac0363c68acfac0cae5d0b3a00a9
Alexey Khromets <alexchromets@gmail.com>
2017-08-03 14:01:37 +0300

    add items and /api/items route

--- server/server.js
+++ server/server.js
@@ -7,6 +7,34 @@ app.route('/api')
     res.status(200).send('This is the API');
 });
 
+var items =
+    [
+        {
+            name: 'Let It Be',
+            link: 'https://google.com',
+            rank: 1
+        },
+        {
+            name: 'Hey Jude',
+            link: 'https://google.com',
+            rank: 2
+        },
+        {
+            name: 'Get Back',
+            link: 'https://google.com',
+            rank: 3
+        }
+    ];
+
+app.route('/api/items')
+.get(function (req, res) {
+    res.status(200).json(items)
+})
+.post(function (req, res) {
+    
+})
+
+
 app.listen(port, function () {
     console.log('App is listening on port ' + port);
 })
